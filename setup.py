560b59289edc7ce223522e532bf5a3f5af061644
Jacky Wong <colabdog@gmail.com>
2023-07-24 14:40:01 +1000

    update setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='PDFAgent',
-    version='0.0.0',
-    url='https://github.com/mypackage.git',
-    author='Author Name',
-    author_email='author@gmail.com',
-    description='Description of my package',
+    version='0.1.0',
+    url='https://github.com/mr-gpt/PDFAgent',
+    author='Twilix Team',
+    author_email='jacky@twilix.io',
+    description='Production Ready PDF Agent',
     packages=find_packages(),    
-    install_requires=['blitzchain'],
+    install_requires=['blitzchain>=0.8.1'],
 )
